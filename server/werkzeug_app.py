f0ebfa640ffeb5b3ec00175f77531114f7d0a5fe
denis <denniiskim@gmail.com>
2024-02-27 03:56:26 +0300

    completed

--- server/werkzeug_app.py
+++ server/werkzeug_app.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+from werkzeug.wrappers import Request, Response
+
+@Request.application
+def application(request):
+    print(f'This web server is running at {request.remote_addr}')
+    return Response('A WSGI generated this response!')
+
+if __name__ == '__main__':
+    from werkzeug.serving import run_simple
+    run_simple(
+        hostname='localhost',
+        port=5555,
+        application=application
+    )
